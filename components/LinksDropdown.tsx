a42d3c5d0dc7041eff89dc863a273428bff52c99
Marco Castanho <marco.castanho@toptal.com>
2024-02-10 22:20:03 +0000

    links dropdown

--- components/LinksDropdown.tsx
+++ components/LinksDropdown.tsx
@@ -1,7 +1,40 @@
 import React from "react";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "./ui/dropdown-menu";
+import { Button } from "./ui/button";
+import { AlignLeft } from "lucide-react";
+import { links } from "@/utils/links";
+import Link from "next/link";
 
 const LinksDropdown = () => {
-  return <div>LinksDropdown</div>;
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild className="lg:hidden">
+        <Button variant="outline" size="icon">
+          <AlignLeft />
+          <span className="sr-only">Toggle links</span>
+        </Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent
+        className="w-52 lg:hidden"
+        align="start"
+        sideOffset={25}
+      >
+        {links.map((link) => (
+          <DropdownMenuItem key={link.href}>
+            <Link href={link.href} className="flex items-center gap-x-2">
+              {link.icon}
+              <span className="capitalize">{link.label}</span>
+            </Link>
+          </DropdownMenuItem>
+        ))}
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
 };
 
 export default LinksDropdown;
