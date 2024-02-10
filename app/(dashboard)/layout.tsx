cf937ea31819f9ae42dec12f4e417f1ad4ebade2
Marco Castanho <marco.castanho@toptal.com>
2024-02-10 21:59:18 +0000

    dashboard layout

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -1,3 +1,5 @@
+import NavBar from "@/components/NavBar";
+import Sidebar from "@/components/Sidebar";
 import React, { FC, ReactNode } from "react";
 
 interface Props {
@@ -5,7 +7,20 @@ interface Props {
 }
 
 const DashboardLayout: FC<Props> = ({ children }) => {
-  return <div>{children}</div>;
+  return (
+    <main className="grid lg:grid-cols-5">
+      {/* first-col hide on small screen */}
+      <div className="hidden lg:block lg:col-span-1 lg:min-h-screen">
+        <Sidebar />
+      </div>
+
+      {/* second-col hide dropdown on big screen */}
+      <div className="lg:col-span-4">
+        <NavBar />
+        <div className="py-16 px-4 sm:px-8 lg:px-16">{children}</div>
+      </div>
+    </main>
+  );
 };
 
 export default DashboardLayout;
