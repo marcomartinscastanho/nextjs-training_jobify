5246922ae5f9d8c08505f00451b9dda2c51e3b0c
Marco Castanho <marco.castanho@toptal.com>
2024-02-11 14:01:37 +0000

    create job form

--- components/CreateJobForm.tsx
+++ components/CreateJobForm.tsx
@@ -4,43 +4,67 @@ import React from "react";
 import { useForm } from "react-hook-form";
 import * as z from "zod";
 import { zodResolver } from "@hookform/resolvers/zod";
-import {
-  Form,
-  FormControl,
-  FormField,
-  FormItem,
-  FormLabel,
-  FormMessage,
-} from "./ui/form";
-import { Input } from "./ui/input";
 import { Button } from "./ui/button";
-import { CustomFormField } from "./FormComponents";
-
-const formSchema = z.object({
-  username: z.string().min(2, {
-    message: "Username must be at least 2 characters.",
-  }),
-});
+import { CustomFormField, CustomFormSelect } from "./FormComponents";
+import { Form } from "./ui/form";
+import {
+  CreateAndEditJobType,
+  JobMode,
+  JobStatus,
+  createAndEditJobSchema,
+} from "@/types/job";
 
 const CreateJobForm = () => {
   // 1. Define the form
-  const form = useForm<z.infer<typeof formSchema>>({
-    resolver: zodResolver(formSchema),
+  const form = useForm<CreateAndEditJobType>({
+    resolver: zodResolver(createAndEditJobSchema),
     defaultValues: {
-      username: "",
+      position: "",
+      company: "",
+      location: "",
+      status: JobStatus.Pending,
+      mode: JobMode.FullTime,
     },
   });
 
   // 2. Define a submit handler
-  const handleSubmit = (values: z.infer<typeof formSchema>) => {
+  const handleSubmit = (values: CreateAndEditJobType) => {
     console.log(values);
   };
 
   return (
     <Form {...form}>
-      <form onSubmit={form.handleSubmit(handleSubmit)} className="space-y-8">
-        <CustomFormField name="username" control={form.control} />
-        <Button type="submit">Submit</Button>
+      <form
+        onSubmit={form.handleSubmit(handleSubmit)}
+        className="bg-muted p-8 rounded"
+      >
+        <h2 className="capitalize font-semibold text-4xl mb-6">add job</h2>
+        <div className="grid gap-4 mf:grid-cols-2 lg:grid-cols-3 items-start">
+          {/* position */}
+          <CustomFormField name="position" control={form.control} />
+          {/* company */}
+          <CustomFormField name="company" control={form.control} />
+          {/* location */}
+          <CustomFormField name="location" control={form.control} />
+          {/* job status */}
+          <CustomFormSelect
+            name="status"
+            control={form.control}
+            labelText="job status"
+            items={Object.values(JobStatus)}
+          />
+          {/* job mode */}
+          <CustomFormSelect
+            name="mode"
+            control={form.control}
+            labelText="job mode"
+            items={Object.values(JobMode)}
+          />
+          {/* button */}
+          <Button type="submit" className="self-end capitalize">
+            submit
+          </Button>
+        </div>
       </form>
     </Form>
   );
