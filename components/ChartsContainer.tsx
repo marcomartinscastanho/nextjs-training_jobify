2b9734638086aaee8d35f229c17400a5b6872a38
Marco Castanho <marco.castanho@toptal.com>
2024-02-12 12:54:49 +0000

    charts container

--- components/ChartsContainer.tsx
+++ components/ChartsContainer.tsx
@@ -1,7 +1,39 @@
+"use client";
+import { getChartsDataAction } from "@/utils/actions";
+import { useQuery } from "@tanstack/react-query";
 import React from "react";
+import {
+  Bar,
+  BarChart,
+  CartesianGrid,
+  ResponsiveContainer,
+  Tooltip,
+  XAxis,
+  YAxis,
+} from "recharts";
 
 const ChartsContainer = () => {
-  return <h2>ChartsContainer</h2>;
+  const { data } = useQuery({
+    queryKey: ["charts"],
+    queryFn: () => getChartsDataAction(),
+  });
+
+  if (!data || data.length < 1) return null;
+
+  return (
+    <section className="mt-16">
+      <h1 className="text-4xl font-semibold text-center">Monthly Applications</h1>
+      <ResponsiveContainer width="100%" height={300}>
+        <BarChart data={data} margin={{ top: 50 }}>
+          <CartesianGrid strokeDasharray="3 3" />
+          <XAxis dataKey="date" />
+          <YAxis allowDecimals={false} />
+          <Tooltip />
+          <Bar dataKey="count" fill="#2563eb" barSize={75} />
+        </BarChart>
+      </ResponsiveContainer>
+    </section>
+  );
 };
 
 export default ChartsContainer;
